92f173470c8c714caf08355fc8f5c52c7037e552
cclauss <cclauss@bluewin.ch>
2016-03-20 18:39:40 +0100

    Update black_white.py

--- black_white.py
+++ black_white.py
@@ -420,7 +420,7 @@ class Game (Scene):
 	def winning(self):
 		square_states = [square.state for square in self.squares]
                 self.black_count.text = str(square_states.count(1))
-		self.white_count.text = str(square_states.count(2))
+                self.white_count.text = str(square_states.count(2))
 		add_score = 0
 		for square in self.squares:
 			if square.state >= 3:
