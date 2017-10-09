8fccfe3b3563a34fb8932476188e0168a92a1357
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2017-10-08 22:56:35 -0400

    Updating tabs to spaces

--- highscore_reader.py
+++ highscore_reader.py
@@ -2,10 +2,10 @@ import pickle
 
 # Select difficulty and run script to see high score table
 
-difficulty = 2 # Change this (1-easy, 2-regular, 3-hard)
+difficulty = 2  # Change this (1-easy, 2-regular, 3-hard)
 
 with open("bwsave"+str(difficulty)+".dat", "rb") as in_file:
-	test = pickle.load(in_file)
+    test = pickle.load(in_file)
 
 for x in sorted(test, reverse=True):
-	print('{} - {}'.format(*x))
+    print('{} - {}'.format(*x))
